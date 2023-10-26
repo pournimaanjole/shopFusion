7edfca365b00d586f0dc85c831f601a66d50405a
pournimaanjole <pournimaanjole@gmail.com>
2023-10-26 14:53:57 +0530

    creating signup and login api

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ dotenv.config()
 const app = express();
 app.use(express.json())
 const PORT = process.env.PORT || 5000
+import User from './model/user.js';
 
 
 const connectmongodb = async ()=>{
@@ -13,6 +14,52 @@ const connectmongodb = async ()=>{
         console.log("connected to mongodb");
     }
 }
+
+
+// post request 
+// creating instence
+
+app.post('/users' , async (req,res) =>{
+const {name,email,mobile,password,gender,address} = req.body
+
+const userdata = new User ({
+    name,email,mobile,password,address,gender
+})
+try{
+    const saveduser = await userdata.save();
+res.json({
+    success:true,
+    data:saveduser,
+    message:"succesfully signup"
+})
+
+}
+catch(error){
+    res.json({
+        message:error.message
+    })
+}
+})
+
+app.post("/login" , async(req,res)=>{
+const {email,password} = req.body
+const finduser = await User.findOne({
+    email:email,
+    password:password
+}).select("name email address")
+if(!finduser){
+    return res.json({
+        success:false,
+        message:"email and password are not found"
+    })
+}
+res.json({
+    success:true,
+    data:finduser,
+    meassge:"login successfully"
+})
+})
+
 
 app.listen(PORT, ()=>{
     console.log("server is on ");
---
+++ server/model/user.js
@@ -0,0 +1,32 @@
+import {  Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+    name:{
+        type:String,
+        default:" -"
+    },
+    email:{
+        type:String,
+        required :true,
+        unique:true
+    },
+    mobile:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String
+    },
+    password:{
+        type:Number,
+        required:true,
+      
+    },
+    gender:{
+        type:String,
+        default:"not to say"
+    }
+})
+
+const User = model("User" , userSchema)
+export default User
